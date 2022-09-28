b713153722a8f41884956436338607f77d5cb624
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-09-28 23:54:08 +0200

    chore: Go 1.19 upgrade
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -9,26 +9,19 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, gobin, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
 
           overlays = [
-            (
-              final: prev: {
-                golangci-lint = gobin.packages.${system}.golangci-lint-bin;
-              }
-            )
+            (final: prev: {
+              golangci-lint = gobin.packages.${system}.golangci-lint-bin;
+            })
           ];
         };
 
-        buildDeps = with pkgs; [ git go_1_18 gnumake ];
-        devDeps = with pkgs; buildDeps ++ [
-          golangci-lint
-        ];
-      in
-      { devShell = pkgs.mkShell { buildInputs = devDeps; }; }
-    );
+        buildDeps = with pkgs; [ git go_1_19 gnumake ];
+        devDeps = with pkgs; buildDeps ++ [ golangci-lint ];
+      in { devShell = pkgs.mkShell { buildInputs = devDeps; }; });
 }
